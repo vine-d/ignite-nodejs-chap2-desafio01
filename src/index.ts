2da78ec1a5c95e8c02d08e52094a1713a7d45547
Vinícius Dutra <vinicius.dutra@gmail.com>
2022-05-23 17:34:04 -0300

    Added swaggerUi - openapi documentation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import swagger from "swagger-ui-express";
+
+import swaggerConfigs from "./swaggerConfig.json";
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -7,5 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerConfigs));
 
 export { app };
